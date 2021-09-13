4773bacfd597ba6fee524684738b8148d1802615
TechnoCookiethePerson <66851745+TechnoCookiethePerson@users.noreply.github.com>
2021-09-13 13:24:07 -0300

    HelloWorld
    HelloWorld

--- Program.cs
+++ Program.cs
@@ -10,22 +10,12 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Some Kinda Text Adventure Thing");
-            Console.WriteLine("By Me, Carson MacIntosh <- that's me :)");
-            Console.WriteLine("");
-            Console.WriteLine("--------------------------------------------");
-            Console.WriteLine("New Game     Save    Load");
-            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("Hey");
+            Console.WriteLine("Press a key to see something cool");;
             Console.ReadKey();
             Console.Clear();
-            Console.WriteLine("You wake up 7 o'clock you need to be at work by 9");
-            Console.WriteLine("Do you get out of bed?");
-            Console.WriteLine("");
-            Console.WriteLine("--------------------------------------------");
-            Console.WriteLine("Yes     No");
-            Console.WriteLine("Save     Load");
-            Console.WriteLine("--------------------------------------------");
-            Console.WriteLine("");
+            Console.WriteLine("Boom! Text is gone!");
+            Console.WriteLine("Pretty cool huh?");
             Console.ReadKey();
         }
     }
